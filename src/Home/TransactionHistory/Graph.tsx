5b33736b3225fc238c70947ba5f4d031ff61a168
TahirReyaz <tahir200213@st.jmi.ac.in>
2022-05-20 17:55:43 +0530

    Completed graph bars

--- src/Home/TransactionHistory/Graph.tsx
+++ src/Home/TransactionHistory/Graph.tsx
@@ -1,18 +1,21 @@
-import { View, Text, Dimensions } from "react-native";
+import { View, Dimensions } from "react-native";
 import React from "react";
+
 import { Box, useTheme } from "../../components";
+import { Theme } from "../../components/Theme";
 
 const { width: wWidth } = Dimensions.get("window");
 const aspectRatio = 195 / 305; // This number came from figma design
 
 const lert = (v0: number, v1: number, t: number) => (1 - t) * v0 + t * v1;
 
-interface Point {
+export interface DataPoint {
   date: number;
   value: number;
+  color: keyof Theme["colors"];
 }
 interface GraphProps {
-  data: Point[];
+  data: DataPoint[];
 }
 
 const Graph = ({ data }: GraphProps) => {
@@ -28,7 +31,7 @@ const Graph = ({ data }: GraphProps) => {
   const maxY = Math.max(...values);
 
   return (
-    <Box width={width} height={height} marginTop="m">
+    <Box width={width} height={height} marginTop="xl">
       {data.map((point, i) => {
         if (point.value === 0) {
           return null;
@@ -41,8 +44,28 @@ const Graph = ({ data }: GraphProps) => {
             bottom={0}
             width={step}
             height={lert(0, height, point.value / maxY)}
-            backgroundColor="primary"
-          />
+          >
+            <Box
+              position="absolute"
+              top={0}
+              bottom={0}
+              left={theme.spacing.m}
+              right={theme.spacing.m}
+              backgroundColor={point.color}
+              opacity={0.1}
+              borderTopLeftRadius="m"
+              borderTopRightRadius="m"
+            />
+            <Box
+              position="absolute"
+              top={0}
+              height={32}
+              left={theme.spacing.m}
+              right={theme.spacing.m}
+              backgroundColor={point.color}
+              borderRadius="m"
+            />
+          </Box>
         );
       })}
     </Box>
--- src/Home/TransactionHistory/TransactionHistory.tsx
+++ src/Home/TransactionHistory/TransactionHistory.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import { Box, Button, Header, Text } from "../../components";
 import { HomeNavigationProps } from "../../components/Navigation";
-import Graph from "./Graph";
+import Graph, { DataPoint } from "./Graph";
 
-const data = [
-  { date: new Date("2021-09-01").getTime(), value: 0 },
-  { date: new Date("2021-10-01").getTime(), value: 0 },
-  { date: new Date("2021-11-01").getTime(), value: 139.42 },
-  { date: new Date("2021-12-01").getTime(), value: 281.23 },
-  { date: new Date("2022-01-01").getTime(), value: 0 },
-  { date: new Date("2022-02-01").getTime(), value: 198.54 },
-  { date: new Date("2022-03-01").getTime(), value: 0 },
+const data: DataPoint[] = [
+  { date: new Date("2021-09-01").getTime(), value: 0, color: "primary" },
+  { date: new Date("2021-10-01").getTime(), value: 0, color: "primary" },
+  { date: new Date("2021-11-01").getTime(), value: 139.42, color: "primary" },
+  { date: new Date("2021-12-01").getTime(), value: 281.23, color: "orange" },
+  { date: new Date("2022-01-01").getTime(), value: 0, color: "primary" },
+  { date: new Date("2022-02-01").getTime(), value: 198.54, color: "yellow" },
+  { date: new Date("2022-03-01").getTime(), value: 0, color: "primary" },
 ];
 
 const TransactionHistory = ({
